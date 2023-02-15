caf549b50f98ea2391d84c6beb08304a01cf623b
Anastasiia <dessertkiev@gmail.com>
2023-02-15 22:43:05 +0200

    01-color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+startBtn.addEventListener('click', onClick);
 
+function onClick() {
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startBtn.setAttribute('disabled', 'disabled');
+  stopBtn.removeAttribute('disabled', 'disabled');
+}
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+  stopBtn.setAttribute('disabled', 'disabled');
+  startBtn.removeAttribute('disabled', 'disabled');
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
