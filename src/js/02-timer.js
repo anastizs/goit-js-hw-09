38788a8b73200911d7a1f5e9ca9fc9da07de0c2c
Anastasiia <dessertkiev@gmail.com>
2023-02-26 19:09:55 +0200

    update 02-timer

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -4,18 +4,19 @@ import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import Notiflix from 'notiflix';
 
+const input = document.querySelector('input#datetime-picker');
+const button = document.querySelector('[data-start]');
+
+// button.disabled = false;
+
 const refs = {
-  inputEl: document.querySelector('#datetime-picker'),
-  btnStart: document.querySelector('[data-start]'),
   days: document.querySelector('[data-days]'),
   hours: document.querySelector('[data-hours]'),
   minutes: document.querySelector('[data-minutes]'),
   seconds: document.querySelector('[data-seconds]'),
 };
 
-let timerId = null;
 let timeData = null;
-refs.btnStart.disabled = true;
 
 const options = {
   enableTime: true,
@@ -26,19 +27,20 @@ const options = {
   onClose(selectedDates) {
     timeData = selectedDates[0].getTime();
 
-    if (timeData < new Date()) {
+    if (timeData < new Date().getTime()) {
       Notiflix.Notify.failure('Please choose a date in the future');
+      button.setAttribute('disabled', true);
       return;
     }
-    refs.btnStart.disabled = false;
+    button.removeAttribute('disabled');
   },
 };
+flatpickr(input, options);
 
-flatpickr(refs.inputEl, options);
+button.setAttribute('disabled', true);
+button.addEventListener('click', onStart);
 
-refs.btnStart.addEventListener('click', startButton);
-
-function startButton() {
+function onStart() {
   timerId = setInterval(() => {
     const deltaTime = timeData - new Date().getTime();
 
@@ -46,33 +48,40 @@ function startButton() {
       clearInterval(timerId);
       return;
     }
-
     const time = convertMs(deltaTime);
     updateClockInfo(time);
   }, 1000);
 
-  refs.btnStart.disabled = true;
+  button.setAttribute('disabled', true);
 }
 
 function updateClockInfo({ days, hours, minutes, seconds }) {
-  refs.days.textContent = addLeadingZero(days);
-  refs.hours.textContent = addLeadingZero(hours);
-  refs.minutes.textContent = addLeadingZero(minutes);
-  refs.seconds.textContent = addLeadingZero(seconds);
+  refs.days.textContent = `${days}`;
+  refs.hours.textContent = `${hours}`;
+  refs.minutes.textContent = `${minutes}`;
+  refs.seconds.textContent = `${seconds}`;
 }
 
 function convertMs(ms) {
+  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
-  const days = Math.floor(ms / day);
-  const hours = Math.floor((ms % day) / hour);
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  // Remaining days
+  const days = addLeadingZero(Math.floor(ms / day));
+  // Remaining hours
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  // Remaining seconds
+  const seconds = addLeadingZero(
+    Math.floor((((ms % day) % hour) % minute) / second)
+  );
+
   return { days, hours, minutes, seconds };
 }
-
 function addLeadingZero(value) {
   return String(value).padStart(2, '0');
 }
