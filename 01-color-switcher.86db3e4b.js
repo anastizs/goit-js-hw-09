07d10b958e418b66848bfa37ed88dc2a13966153
anastizs <104034931+anastizs@users.noreply.github.com>
2023-02-26 19:59:04 +0000

    Deploying to gh-pages from @ anastizs/goit-js-hw-09@e244d28ffc81222986fe1debbfac8f844fbbdc5f 🚀

--- 01-color-switcher.86db3e4b.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");t.addEventListener("click",(function(){timerId=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.setAttribute("disabled"),e.removeAttribute("disabled")})),e.addEventListener("click",(function(){clearInterval(timerId),e.setAttribute("disabled"),t.removeAttribute("disabled")}))}();
-//# sourceMappingURL=01-color-switcher.86db3e4b.js.map
